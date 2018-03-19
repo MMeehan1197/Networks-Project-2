fc5170995af90605261f727ee633cfcdac665b01
Frank Hulmes <fdhulmes@udel.edu>
2018-03-18 21:40:24 -0400

    Added client and server

---
+++ tcpclient.c
@@ -0,0 +1,122 @@
+/* tcp_ client.c */
+/* Programmed by Adarsh Sethi */
+/* February 21, 2018 */
+
+#include <stdio.h>          /* for standard I/O functions */
+#include <stdlib.h>         /* for exit */
+#include <string.h>         /* for memset, memcpy, and strlen */
+#include <netdb.h>          /* for struct hostent and gethostbyname */
+#include <sys/socket.h>     /* for socket, connect, send, and recv */
+#include <netinet/in.h>     /* for sockaddr_in */
+#include <unistd.h>         /* for close */
+
+#define STRING_SIZE 1024
+
+struct header {
+   short packet_sequence;
+   short msg_len;
+};
+
+int main(void) {
+
+   int sock_client;  /* Socket used by client */
+
+   struct sockaddr_in server_addr;  /* Internet address structure that
+                                        stores server address */
+   struct hostent * server_hp;      /* Structure to store server's IP
+                                        address */
+   char server_hostname[STRING_SIZE]; /* Server's hostname */
+   unsigned short server_port;  /* Port number used by server (remote port) */
+
+   char filename[STRING_SIZE];  /* send message */
+   char modifiedSentence[STRING_SIZE]; /* receive message */
+   unsigned int msg_len;  /* length of message */
+   int bytes_sent, bytes_recd; /* number of bytes sent or received */
+   short packet_sequence; /* Packet Sequence number */
+
+   /* open a socket */
+
+   if ((sock_client = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+      perror("Client: can't open stream socket");
+      exit(1);
+   }
+
+   /* Note: there is no need to initialize local client address information
+            unless you want to specify a specific local port
+            (in which case, do it the same way as in udpclient.c).
+            The local address initialization and binding is done automatically
+            when the connect function is called later, if the socket has not
+            already been bound. */
+
+   /* initialize server address information */
+
+   printf("Enter hostname of server: ");
+   scanf("%s", server_hostname);
+   if ((server_hp = gethostbyname(server_hostname)) == NULL) {
+      perror("Client: invalid server hostname");
+      close(sock_client);
+      exit(1);
+    }
+
+       printf("Enter port number for server: ");
+       scanf("%hu", &server_port);
+
+       /* Clear server address structure and initialize with server address */
+       memset(&server_addr, 0, sizeof(server_addr));
+       server_addr.sin_family = AF_INET;
+       memcpy((char *)&server_addr.sin_addr, server_hp->h_addr,
+                                        server_hp->h_length);
+       server_addr.sin_port = htons(server_port);
+
+        /* connect to the server */
+
+       if (connect(sock_client, (struct sockaddr *) &server_addr,
+                                        sizeof (server_addr)) < 0) {
+          perror("Client: can't connect to server");
+          close(sock_client);
+          exit(1);
+       }
+
+       /* user interface */
+
+       printf("Please enter the filename:\n");
+       scanf("%s", filename);
+       msg_len = strlen(filename) + 1;
+
+       struct header packetheader = {packet_sequence = 0, msg_len = sizeof(filename)};
+
+       /* send message */
+       printf("1");
+         bytes_sent = send(sock_client, &packetheader, sizeof(&packetheader), 0);
+         printf("\n%lu", sizeof(&packetheader));
+         bytes_sent = send(sock_client, filename, msg_len, 0);
+         printf("3");
+        /* Open the output file */
+
+         FILE* output = fopen("out.txt", "w");
+
+         /* get response from server */
+         char line[80];
+         for ( ; ; ){
+            bytes_recd = recv(sock_client, &packetheader, 8, 0);
+            if (bytes_recd <= 0){
+                break;
+            }
+            packetheader.packet_sequence  = ntohs(packetheader.packet_sequence);
+            packetheader.msg_len  = ntohs(packetheader.msg_len);
+            int line_size = packetheader.msg_len;
+            if (line_size == 0){
+                break;
+            }
+            bytes_recd = recv(sock_client, line, line_size, 0);
+            if (bytes_recd <= 0){
+                break;
+            }
+            fputs(line, output);
+
+
+      }
+      /* close the socket */
+
+        close (sock_client);
+     }
---
+++ tcpserver.c
@@ -0,0 +1,159 @@
+/* tcpserver.c */
+/* Programmed by Adarsh Sethi */
+/* February 21, 2018 */
+
+#include <ctype.h>          /* for toupper */
+#include <stdio.h>          /* for standard I/O functions */
+#include <stdlib.h>         /* for exit */
+#include <string.h>         /* for memset */
+#include <sys/socket.h>     /* for socket, bind, listen, accept */
+#include <netinet/in.h>     /* for sockaddr_in */
+#include <unistd.h>         /* for close */
+
+#define STRING_SIZE 1024
+
+/* SERV_TCP_PORT is the port number on which the server listens for
+   incoming requests from clients. You should change this to a different
+   number to prevent conflicts with others in the class. */
+
+#define SERV_TCP_PORT 46920
+
+/* Defining the struct for the header */
+
+struct header{
+        short packet_sequence;
+        short msg_len;
+};
+
+int main(void) {
+
+   int sock_server;  /* Socket on which server listens to clients */
+   int sock_connection;  /* Socket on which server exchanges data with client */
+
+   struct sockaddr_in server_addr;  /* Internet address structure that
+                                        stores server address */
+   unsigned int server_addr_len;  /* Length of server address structure */
+   unsigned short server_port;  /* Port number used by server (local port) */
+
+   struct sockaddr_in client_addr;  /* Internet address structure that
+                                        stores client address */
+   unsigned int client_addr_len;  /* Length of client address structure */
+
+   char filename[STRING_SIZE];  /* receive message */
+   char modifiedSentence[STRING_SIZE]; /* send message */
+   unsigned int msg_len;  /* length of message */
+   int bytes_sent, bytes_recd; /* number of bytes sent or received */
+   unsigned int i;  /* temporary loop variable */
+   struct header* packetheader = malloc(8);
+
+   /* open a socket */
+
+   if ((sock_server = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP)) < 0) {
+      perror("Server: can't open stream socket");
+      exit(1);
+   }
+
+   /* initialize server address information */
+
+   memset(&server_addr, 0, sizeof(server_addr));
+ server_addr.sin_family = AF_INET;
+   server_addr.sin_addr.s_addr = htonl (INADDR_ANY);  /* This allows choice of
+                                        any host interface, if more than one
+                                        are present */
+   server_port = SERV_TCP_PORT; /* Server will listen on this port */
+   server_addr.sin_port = htons(server_port);
+
+   /* bind the socket to the local server port */
+
+   if (bind(sock_server, (struct sockaddr *) &server_addr,
+                                    sizeof (server_addr)) < 0) {
+      perror("Server: can't bind to local address");
+      close(sock_server);
+      exit(1);
+   }
+
+   /* listen for incoming requests from clients */
+
+   if (listen(sock_server, 50) < 0) {    /* 50 is the max number of pending */
+      perror("Server: error on listen"); /* requests that will be queued */
+      close(sock_server);
+      exit(1);
+   }
+   printf("I am here to listen ... on port %hu\n\n", server_port);
+
+   client_addr_len = sizeof (client_addr);
+
+   /* wait for incoming connection requests in an indefinite loop */
+
+   for (;;) {
+ sock_connection = accept(sock_server, (struct sockaddr *) &client_addr,
+                                         &client_addr_len);
+                     /* The accept function blocks the server until a
+                        connection request comes from a client */
+      if (sock_connection < 0) {
+         perror("Server: accept() error\n");
+         close(sock_server);
+         exit(1);
+      }
+
+      /* receive the message */
+
+      bytes_recd = recv(sock_connection, packetheader, 8, 0);
+      printf("1");
+      packetheader->packet_sequence  = ntohs(packetheader->packet_sequence);
+      printf("2");
+      packetheader->msg_len  = ntohs(packetheader->msg_len);
+      int filename_size = packetheader->msg_len;
+      printf("3");
+      bytes_recd = recv(sock_connection, filename, filename_size, 0);
+      /* Opening the file */
+
+      FILE *file = fopen(filename, "r");
+
+      int count, c ;
+      char current_line[80];
+      short packet_sequence = packetheader->packet_sequence;
+      short msg_len = packetheader->msg_len;
+      count = 0;
+for( ; ; ){
+        c = fgetc( file);
+        if(c == EOF){
+           packet_sequence += 1;
+           msg_len = count;
+           packetheader->packet_sequence  = htons(packet_sequence);
+           packetheader->msg_len  = htons(msg_len);
+           bytes_sent = send(sock_connection, &packetheader, 8, 0);
+           printf("Line");
+           bytes_sent = send(sock_connection, current_line, msg_len + 1, 0);
+           break;
+        }
+        else if(c == '\n'){
+           current_line[count] =(char) c;
+           count++;
+           packet_sequence += 1;
+           msg_len = count;
+           packetheader->packet_sequence  = htons(packet_sequence);
+           packetheader->msg_len  = htons(msg_len);
+           bytes_sent = send(sock_connection, &packetheader, 8, 0);
+           printf("Line");
+           bytes_sent = send(sock_connection, current_line, msg_len + 1, 0);
+           count = 0;
+        }
+        else{
+           current_line[count] = (char) c;
+           count++;
+         }
+     }
+ /* Sending End of transmission packet */
+
+     packetheader->packet_sequence += 1;
+     packetheader->msg_len = 0;
+     bytes_sent = send(sock_connection, &packetheader, 8, 0);
+
+      /* close the socket */
+
+      close(sock_connection);
+   }
+}
+
+
